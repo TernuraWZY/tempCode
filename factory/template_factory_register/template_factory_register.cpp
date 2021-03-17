807fa0158722991422df48f3d29ff133d75a3b9e
TernuraWZY <623510156@qq.com>
2021-03-17 10:08:00 +0800

    git push

--- factory/template_factory_register/template_factory_register.cpp
+++ factory/template_factory_register/template_factory_register.cpp
@@ -4,9 +4,12 @@ int main()
 {
     // 1. 注册产品
     productRegister<Shoes, NiKeShoes> NIKEShoesRegister("nike");
+    productRegister<Clothe, UniqloClothe> UniqloRegister("Uniqlo");
     // 获取工厂实例
     Shoes *NIKE = abstractFactory<Shoes>::getInstance()->findObject("nike");
     NIKE->Show();
     
+    Clothe *uniqlo = abstractFactory<Clothe>::getInstance()->findObject("Uniqlo");
+    uniqlo->Show();
     system("pause");
 }
