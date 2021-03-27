b7a96a53083340c070046715884ba3b6ac802243
TernuraWZY <67008011+TernuraWZY@users.noreply.github.com>
2021-03-28 00:06:26 +0800

    -a

---
+++ strcpy/strcpy.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <assert.h>
+#include <cstring>
+#include <stdlib.h>
+using namespace std;
+
+char *m_strcpy(char *strDest, const char *strSrc)
+{
+    assert(strDest != NULL && strSrc != NULL);
+    char *addr = strDest;
+    while (*strDest = *strSrc)
+    {
+        strDest++;
+        strSrc++;
+    }
+    return addr;
+}
+
+int main()
+{
+    char *dest = (char *)malloc(100*sizeof(char));
+    cout << m_strcpy(dest, "7189413123abc123") << endl;
+    cout << strlen(dest) << endl;
+}
+
